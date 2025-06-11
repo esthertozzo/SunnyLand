8eed2a375af34f06f4c8b0dbb376e34343a54393
esther <tozzo.est@gmail.com>
2025-06-11 09:22:21 -0300

    introdução a C#

---
+++ SunnyLand/Assets/Scripts/Coletavel.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coletavel : MonoBehaviour
+{
+    private int pontos = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("o jogo começou! com " + pontos + " pontos.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       // Debug.Log("rodando!");
+    }
+}
---
+++ SunnyLand/Assets/Scripts/Personagem.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Personagem : MonoBehaviour
+{
+    public string nome = "remi";
+    public int vidaInicial = 1;
+    public float velocidadeMovimento = 2.5f;
+    public bool isDead = false;
+    private Rigidbody2D rb;
+    public float novaGravidade = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Nome:\n" + nome + "Pontos:\n" + vidaInicial);
+        rb = GetComponent<Rigidbody2D>();
+        rb.gravityScale = novaGravidade;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
